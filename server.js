3e043763b3d0c9ae5ab7692be4655acccfe784d5
Shehab Tarek <shehab.tarek@intellisc.com>
2020-02-19 08:15:23 +0200

    asdasd
    sadasd

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/front-end/dist/'));
+app.use(express.static(__dirname + '/angularapp'));
 app.get('/*', function(req,res){
-  res.sendFile('./front-end/dist/index.html');
+  res.sendFile(path.join(__dirname, '/angularapp/index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
