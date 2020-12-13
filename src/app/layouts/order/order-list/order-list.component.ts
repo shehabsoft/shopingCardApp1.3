a6edb56e3dd0bfcfc0a6876ff3d199d6dc6ab066
shehab tarek <shehab.tarek@intellisc.com>
2020-12-13 10:10:24 +0200

    order Pricing
    order Pricing

--- src/app/layouts/order/order-list/order-list.component.ts
+++ src/app/layouts/order/order-list/order-list.component.ts
@@ -3,6 +3,8 @@ import { OrderService } from 'src/app/shared/services/order.service';
  
 import { AuthServiceLocal } from 'src/app/shared/services/auth.service.local';
 import { User } from 'src/app/shared/models/user';
+import { Order } from 'src/app/shared/models/order';
+import { OrdersProducts } from 'src/app/shared/models/ordersProducts';
 
 @Component({
   selector: 'app-order-list',
@@ -14,10 +16,37 @@ export class OrderListComponent implements OnInit {
   constructor(private orderService: OrderService, private authService: AuthServiceLocal) { }
   orderId: number;
   user: User;
+  orders: Order[];
+  totalSellerValue = 0;
+  orderProfitValue = 0;
+  packagePriceValue = 0;
+  totalCleaning = 0;
   ngOnInit() {
     this.user = this.authService.getLoggedInUser();
     this.orderService.getOrders(this.user.id).subscribe(response => {
       console.log(response);
+      this.orders = response;
+      this.totalSellerValue = 0;
+      this.orderProfitValue = 0;
+      this.packagePriceValue = 0;
+      this.totalCleaning = 0;
+      this.orders.forEach(order => {
+        const products: OrdersProducts[] = order.ordersProducts;
+ 
+        products.forEach((productseller) => {
+          this.packagePriceValue += (productseller.quantity * 5);
+          if (productseller.cleaningFee.id != 3) {//  3 is id for no cleaning
+            this.totalSellerValue += productseller.product.sellerPrice * productseller.quantity;
+            this.totalCleaning += (productseller.quantity * 5);
+          } else {
+            this.totalSellerValue += productseller.product.sellerPrice * productseller.quantity
+          }
+          this.orderProfitValue += (productseller.product.price * productseller.quantity) - (productseller.product.sellerPrice * productseller.quantity)
+
+        });
+
+      });
+      
 
     });
     this.orderService.getOrder(this.orderId).subscribe(response => {
